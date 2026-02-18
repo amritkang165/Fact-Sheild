9c649187bbea2ce0f84e23b0efb27419497984e7
Vanshika Asati <YOUR_GITHUB_EMAIL@example.com>
2026-02-18 19:18:44 +0530

    final cover

--- Frontend/FactShield/src/pages/Home.jsx
+++ Frontend/FactShield/src/pages/Home.jsx
@@ -1,13 +1,30 @@
 function Home() {
   return (
-    <div style={{ width: "100vw", height: "100vh" }}>
+    <div className="hero">
+      {/* Spline Background */}
       <spline-viewer
         url="https://prod.spline.design/zdsDt12YtbazL2tv/scene.splinecode"
-        style={{ width: "100%", height: "100%", display: "block" }}
+        className="spline-bg"
       ></spline-viewer>
+
+      {/* React Overlay Content */}
+      <div className="hero-content">
+        <h1>
+          Fight <br />
+          <span>Misinformation</span> <br />
+          With AI
+        </h1>
+
+        <p>
+          Instantly verify news, articles, and social media posts.
+          <br />
+          Get accurate, AI-powered credibility insights in seconds.
+        </p>
+
+        <button>CHECK NEWS NOW</button>
+      </div>
     </div>
   )
 }
 
 export default Home
-
